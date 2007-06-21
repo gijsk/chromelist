c8357e2ac44682356afcbd37cd68c04e985d1a67
GijsKruitbosch <GijsKruitbosch@652e530c-8f2f-0410-a032-d38e632a91c5>
2007-06-21 19:34:34 +0000

    Beter bat script
    git-svn-id: https://chromelist.googlecode.com/svn/trunk@10 652e530c-8f2f-0410-a032-d38e632a91c5

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 del chromelist.xpi
-rem this is fugly, but who cares.
-ls | sed s/build\.bat// | sed s/preview.png// | sed s/preview-small.png// | xargs 7z a -tzip "chromelist.xpi" -r -mx=9 -x!*CVS*
+zip -r chromelist.xpi content/ skin/ locale/ defaults/ license.txt chrome.manifest install.rdf -x *.svn*
+
