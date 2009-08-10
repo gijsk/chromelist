dc80bce1fcb9012df779edcfc3aac1baac15d5bb
GijsKruitbosch <GijsKruitbosch@652e530c-8f2f-0410-a032-d38e632a91c5>
2009-08-10 16:26:41 +0000

    Wrong variable name, oops...
    git-svn-id: https://chromelist.googlecode.com/svn/trunk@59 652e530c-8f2f-0410-a032-d38e632a91c5

--- content/lib/parser.js
+++ content/lib/parser.js
@@ -492,9 +492,9 @@ function jarChildrenGenerator(chromeStructure, cDir, realURL)
             }
             else if (ex.result == Components.results.NS_ERROR_FILE_NOT_FOUND)
             {
-                desc = getStr("problem.noJarFile", [cDir.href,
-                                                       jarFileURL.spec]);
-                prob = {desc: desc, manifest: manifest, severity: "error"};
+                desc = getStr("problem.noJarFile", [cDir.href, realURL.spec]);
+                prob = {desc: desc, manifest: manifest, severity: "error",
+                        url: cDir.href, flags: cDir.flags};
                 chromeBrowser.addProblem(prob);
                 delete cDir.parent.directories[cDir.leafName];
             }
